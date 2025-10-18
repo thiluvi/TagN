c03a862739d59b4226f0eb5f13c00c8222bfc09c
thiluvi <thiagoluvizutto11@gmail.com>
2025-10-18 01:02:36 -0300

    Segundo commit com novos components: footer, info-strip, hero-banner.
    Topbar atualizado.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Topbar } from './topbar/topbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Topbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
