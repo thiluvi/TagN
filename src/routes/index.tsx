5be3751827b07305d632cedae01ad7e1c386fecc
Nicolas Azevedo <164301887+azvwdo@users.noreply.github.com>
2026-04-08 22:27:05 -0300

    Tela inicial add com Sucesso!

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Importando nossas telas
 import { Login } from '../pages/Login';
 import { Cadastro } from '../pages/Cadastro';
+import { Home } from '../pages/Home';
 
 const Stack = createNativeStackNavigator();
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      {/* initialRoute como "home" para definir a nossa tela inicial */}
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
       </Stack.Navigator>
